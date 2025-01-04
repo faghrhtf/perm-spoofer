ac1bb814738429955a0e91105568ca01d936f9f1
faghrhtf <faghrhtf@gmail.com>
2025-01-04 22:32:36 +0300

    randstr.bat

--- randstr.bat
+++ randstr.bat
@@ -1,29 +1,37 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:: التحقق من وجود إدخال طول السلسلة
 if "%1"=="" (
     echo Usage: %~nx0 [length] [/v]
     echo Example: %~nx0 10 /v
     goto :eof
 )
 
+:: تعيين الطول والمدخلات الأخرى
 set "length=%1"
 set "exclude_volume=%2"
 
+:: تحديد الأحرف المسموح بها
 set "chars=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-if "%exclude_volume%"=="/v" (
+if /i "%exclude_volume%"=="/v" (
     set "chars=ABCDEF0123456789"
 )
 
-set "randomStr="
-
-for /L %%i in (1,1,%length%) do (
-    set /a "index=!random! %% 16"
-    for %%a in (!index!) do (
-        set "randomStr=!randomStr!!chars:~%%a,1!"
-    )
+:: التحقق من أن الطول هو رقم صالح
+for /f "tokens=* delims=0123456789" %%A in ("%length%") do (
+    echo Error: Length must be a number.
+    goto :eof
 )
 
-echo !randomStr!
+:: إنشاء السلسلة العشوائية
+set "randomStr="
+for /L %%i in (1,1,%length%) do (
+    set /a "index=!random! %% %len_chars%"
+    set "randomStr=!randomStr!!chars:~!index!,1!"
+)
+
+:: عرض النتيجة
+echo Random String: !randomStr!
 
 exit /b
